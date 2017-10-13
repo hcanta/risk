694c455a13970072ad9c8c3f73e4e249dfec42f7
hcanta <henry.michel.c@gmail.com>
2017-10-13 15:55:17 -0400

    Missing Enum Class

---
+++ Risk/src/risk/util/RiskEnum.java
@@ -0,0 +1,15 @@
+package risk.util;
+
+public class RiskEnum {
+
+	public static enum RiskEvent
+	{
+		Reinforce, Fortify, Attack
+	}
+	
+	public static enum RiskColor
+	{
+		Red, Green, Yellow, Blue
+	}
+
+}
