742740daef8c5b0bd691bce945954e21268f3d36
Akif <akif.hamid33@gmail.com>
2017-10-15 23:53:01 -0400

    test method is added

--- Risk/src/risk/game/cards/DeckTest.java
+++ Risk/src/risk/game/cards/DeckTest.java
@@ -1,29 +1,77 @@
 package risk.game.cards;
 
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import risk.util.map.Country;
 
+/**
+ * This class is a Junit Test Class which checks the method associated with the Deck class.
+ * @author Mohammad Akif Beg
+ * @version 1.0
+ */
 public class DeckTest {
-
-	@Test
-	public void testDeck() {
-		fail("Not yet implemented");
+	
+	/**
+	 * The private member of the TestClass which helps in checking the assertions.
+	 */
+	
+	private Deck testObject;
+	private Country testCountry;
+	private Card testCard;
+	private String checking;
+	private String otherChecking;
+	private ArrayList<Country> temp;
+	private ArrayList<Card> cardObject;
+	private String typeOfCard;
+	
+	/**
+	 * This is a setUp method which gets implemented before every TestMethod
+	 */
+	
+	@Before
+	public void setUp(){
+		temp = new ArrayList<Country>();
+		cardObject = new ArrayList<Card>();
+		temp.add(testCountry);
+		testObject = new Deck(temp);
+		Country testCountry= temp.remove(0);
+		checking = testCountry.getCard();
+		cardObject.add(1,testCard);
+		otherChecking = testCard.getType();
 	}
-
+	
+	/**
+	 * This method tests the draw() function of the deck class.
+	 */
+	
 	@Test
 	public void testDraw() {
-		fail("Not yet implemented");
+		typeOfCard = testObject.draw().toString();
+		Assert.assertNotNull(typeOfCard);
+		Assert.assertEquals(checking, typeOfCard);
 	}
+	
+	/**
+	 * This method tests the draw() function of the deck class.
+	 */
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		testObject = new Deck(temp);
+	    Assert.assertNull(testObject);
 	}
+	
+	/**
+	 * This method tests the Shuffle method of the deck class.
+	 */
 
 	@Test
 	public void testShuffle() {
-		fail("Not yet implemented");
+		testObject = new Deck(temp);
+		Assert.assertNotNull(testObject);
+		Assert.assertEquals(otherChecking, "Infantry");
 	}
-
 }
