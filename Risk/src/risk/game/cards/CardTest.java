7e1ff34abd5cdc26af2f396d24ff21c1d6565e9c
Akif <akif.hamid33@gmail.com>
2017-10-18 01:22:04 -0400

    TestCase class for Card class

---
+++ Risk/src/risk/game/cards/CardTest.java
@@ -0,0 +1,29 @@
+package risk.game.cards;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCountry() {
+		fail("Not yet implemented");
+	}
+
+}
