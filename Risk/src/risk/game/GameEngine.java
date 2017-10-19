809292e93568dfb5631ca431e2e42773c00c456a
hcanta <henry.michel.c@gmail.com>
2017-10-19 15:13:25 -0400

    Game Engine Update

--- Risk/src/risk/game/GameEngine.java
+++ Risk/src/risk/game/GameEngine.java
@@ -4,10 +4,12 @@
 package risk.game;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import risk.model.character.ICharacter;
 import risk.model.character.PlayerModel;
 import risk.util.RiskEnum.RiskColor;
+import risk.util.map.RiskBoard;
 
 /**
  * @author hcanta
@@ -71,9 +73,26 @@ public class GameEngine
 		}
 	}
 	
-	public Stroing randomAssignCountries()
+	public void randomAssignTerritories()
 	{
+		ArrayList<String> countries = RiskBoard.Instance.getTerritories();
+		Collections.shuffle(countries);
 		
+		ArrayList<ICharacter> pl  = new ArrayList<ICharacter>();
+		for(int i =0; i< players.size(); i++)
+		{
+			pl.add(players.get(i));
+		}
+		
+		Collections.shuffle(pl);
+		for(int i=0; i< countries.size(); i++)
+		{
+			int player = i%pl.size();
+			
+			pl.get(player).addTerritory(countries.get(i));
+			RiskBoard.Instance.getTerritory(countries.get(i));
+		}
+
 	}
 
 		
