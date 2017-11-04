e91d52633640080a79e04f94856c74d80484eb08
Akif <akif.hamid33@gmail.com>
2017-11-04 15:31:19 -0400

    Added Hand Class

---
+++ Risk/src/risk/game/cards/HandTest.java
@@ -0,0 +1,39 @@
+package risk.game.cards;
+
+import junit.framework.TestCase;
+
+public class HandTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testHand() {
+		fail("Not yet implemented");
+	}
+
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	public void testRemoveCardsFromHand() {
+		fail("Not yet implemented");
+	}
+
+	public void testCanTurnInCards() {
+		fail("Not yet implemented");
+	}
+
+	public void testMustTurnInCards() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCards() {
+		fail("Not yet implemented");
+	}
+
+}
