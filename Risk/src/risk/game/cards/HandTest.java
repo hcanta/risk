d109760dd5c5ead70837d7d1cb5f76ea689f1501
Akif <akif.hamid33@gmail.com>
2017-11-07 01:49:31 -0500

    modified handtest

--- Risk/src/risk/game/cards/HandTest.java
+++ Risk/src/risk/game/cards/HandTest.java
@@ -1,39 +1,56 @@
 package risk.game.cards;
 
 import junit.framework.TestCase;
+import risk.util.map.Country;
 
 public class HandTest extends TestCase {
-
+	
+	Card card, card1, card2, card3, card4, card5, card6 ;
+	Hand first;
+	private Country TestObject;
+	
 	protected void setUp() throws Exception {
-		super.setUp();
+		first = new Hand();
+		card = new Card("Infantry",TestObject);
+		card1 = new Card("Cavalry",TestObject);
+		card2 = new Card("Artillery",TestObject);
+		card3 = new Card("Cavalry",TestObject);
+		card4 = new Card("Cavalry",TestObject);
+		card5 = new Card("Artillery",TestObject);
+		card6 = new Card("Infantry",TestObject);
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
 
-	public void testHand() {
-		fail("Not yet implemented");
-	}
-
 	public void testAdd() {
-		fail("Not yet implemented");
+		assertNotNull(first);
+		first.add(card);
+		first.add(card1);
+		first.add(card2);
+		first.add(card3);
+		first.add(card4);
+		first.add(card5);
+		first.add(card6);
 	}
-
-	public void testRemoveCardsFromHand() {
-		fail("Not yet implemented");
+	
+public void testCanTurnInCards() {
+		
+		boolean temp = first.canTurnInCards(1,2,3);
+		assertFalse(temp);
+		
 	}
+public void testRemoveCardsFromHand() {
+	boolean temp = first.getCards().remove(card);
+	assertFalse(temp);
+	boolean temp2 = first.getCards().remove(card);
+	assertFalse(temp2);
+}
 
-	public void testCanTurnInCards() {
-		fail("Not yet implemented");
-	}
-
+	
 	public void testMustTurnInCards() {
-		fail("Not yet implemented");
+		boolean temp = first.mustTurnInCards();
+		assertFalse(temp);			
 	}
-
-	public void testGetCards() {
-		fail("Not yet implemented");
-	}
-
 }
