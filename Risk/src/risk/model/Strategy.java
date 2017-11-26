eb345534bbcadf413e21ee8619a797d0ee392d6b
krn-singh <karan_kalewal@yahoo.co.in>
2017-11-25 20:50:36 -0500

    Strategy model Implementation

---
+++ Risk/src/risk/model/Strategy.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package risk.model;
+
+/**
+ * @author Karanbir Singh
+ *
+ */
+public class Strategy {
+
+	/**
+	 * 
+	 */
+	public Strategy() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
